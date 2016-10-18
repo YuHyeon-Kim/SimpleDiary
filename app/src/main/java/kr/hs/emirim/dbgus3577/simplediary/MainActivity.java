105f2cb566143dda74c0b25a31229dd2abff9191
YuHyeon-Kim <dbgus3577@gmail.com>
2016-10-18 21:01:04 +0900

    #4 button click

--- app/src/main/java/kr/hs/emirim/dbgus3577/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbgus3577/simplediary/MainActivity.java
@@ -1,13 +1,17 @@
 package kr.hs.emirim.dbgus3577.simplediary;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.concurrent.CancellationException;
@@ -17,39 +21,55 @@ public class MainActivity extends AppCompatActivity {
     EditText editDiary;
     Button butSave;
     String fileName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        datePic=(DatePicker)findViewById(R.id.date_picker);
-        editDiary=(EditText)findViewById(R.id.edit_content);
-        butSave=(Button)findViewById(R.id.but_save);
+        datePic = (DatePicker) findViewById(R.id.date_picker);
+        editDiary = (EditText) findViewById(R.id.edit_content);
+        butSave = (Button) findViewById(R.id.but_save);
 
-        Calendar calendar=Calendar.getInstance();
-        int nowYear=calendar.get(Calendar.YEAR);
-        int month=calendar.get(Calendar.MONTH);
-        int date=calendar.get(Calendar.DATE);
+        Calendar calendar = Calendar.getInstance();
+        int nowYear = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
 
         datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                fileName=year+"_"+(monthOfYear+1)+"_"+dayOfMonth+".txt";
-                String content=readDiary(fileName);
+                fileName = year + "_" + (monthOfYear + 1) + "_" + dayOfMonth + ".txt";
+                String content = readDiary(fileName);
                 editDiary.setText(content);
                 butSave.setEnabled(true);
             }
         });
+        butSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream out = openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents = editDiary.getText().toString();
+                    out.write(diaryContents.getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(), "저장이 완료됨", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-    String readDiary(String fileName){
-        String diaryContents=null;
+
+    String readDiary(String fileName) {
+        String diaryContents = null;
         try {
-            FileInputStream in=openFileInput(fileName);
-            byte[] txt=new byte[500];
+            FileInputStream in = openFileInput(fileName);
+            byte[] txt = new byte[500];
             in.read(txt);
             in.close();
-            diaryContents=new String(txt);
+            diaryContents = new String(txt);
             butSave.setText("수정 하기");
-        }catch (IOException e){
+        } catch (IOException e) {
             editDiary.setHint("읽어올 일기가 없음");
             butSave.setText("새로 저장");
         }
